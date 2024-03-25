651b0f09c0b3b273904d85ce516b5eb495f2b251
Vitaly Yurkin <jvn9405@gmail.com>
2024-03-25 09:47:18 +0300

    Поиск элемента

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const users = ['Аня', 'Вика', 'Вика'];
-console.log(users);
-users[2] = 'Кристина';
-console.log(users);
+const roles = ['user', 'admin', 'manager'];
 
-const arrLength = users.push('Никита');
-console.log(users);
-console.log(arrLength);
+const elIndex = roles.indexOf('admin');
+console.log(elIndex);
+const elIndex2 = roles.indexOf('superuser');
+console.log(elIndex2);
 
-const arrLength2 = users.unshift('Вася');
-console.log(users);
-console.log(arrLength2);
+if (roles.indexOf('admin') >=0 ) {
+	console.log('Доступ есть');
+}
 
-const el = users.pop();
-console.log(el);
-console.log(users);
+console.log(roles.includes('admin'));
+console.log(roles.includes('superuser'));
 
-const el2 = users.shift();
-console.log(el2);
-console.log(users);
+if (roles.includes('admin')) {
+	console.log('Доступ есть');
+}
