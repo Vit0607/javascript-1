daaaf1e05eb16f31dec65dc09de389665dc034a6
Vitaly Yurkin <jvn9405@gmail.com>
2024-03-26 09:43:48 +0300

    Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+	Дан список задач
+	const tasks = ['Задача 1'];
+	Сделать функции:
+	- Добавление задачи в конец
+	- Удаление задачи по названию
+	- Перенос задачи в начало списка по названию 
+	Всегда меняем исходный массив
+*/
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
-console.log(url);
+const tasks = ['Задача 1'];
 
-const res2 = roles.join('-');
-console.log(res2);
-console.log(url);
+function Add(task) {
+	tasks.push(task);
+}
+
+function Remove(task) {
+	const index = tasks.indexOf(task);
+	if (index === -1) {
+		return;
+	}
+	tasks.splice(index, 1);
+}
+
+function Prioritize(task) {
+	const index = tasks.indexOf(task);
+	if (index === -1) {
+		return;
+	}
+	const oldTask = tasks[index];
+	tasks.splice(index, 1);
+	tasks.unshift(oldTask);
+}
+
+Add('Задача 2');
+Add('Задача 3');
+console.log(tasks);
+Remove('Задача 4');
+console.log(tasks);
+Prioritize('Задача 4');
+console.log(tasks);
