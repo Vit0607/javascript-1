f328296e9da0566b29fe9544ed75f3592a3d3925
Vitaly Yurkin <jvn9405@gmail.com>
2024-03-30 21:34:31 +0300

    filter

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const transactionsInUSD = [10, -7, 50, -10, 100];
+const operations = [100, -20, 7, -20, 50];
+// const positiveOperations = [];
+// for (const operation of operations) {
+// 	if (operation > 0) {
+// 		positiveOperations.push(operation);
+// 	}
+// }
+// console.log(positiveOperations);
 
-const transactionsInRUB = [];
+const positiveOperations = operations.filter(operation => {
+	return operation > 0;
+});
+console.log(positiveOperations);
 
-for (transaction of transactionsInUSD) {
-	transactionsInRUB.push(transaction * 60);
-}
-
-// console.log(transactionsInUSD);
-// console.log(transactionsInRUB);
-
-const transactionsInRUB2 = transactionsInUSD
-	.map ((transaction, i) => {
-		console.log(i);
-		return transaction * 60;
-	});
-
-console.log(transactionsInUSD);
-console.log(transactionsInRUB2);
+const positiveRUBOperations = operations
+	.filter(operation => {
+		return operation > 0;
+	})
+	.map(operation => operation * 60);
+console.log(positiveRUBOperations);
