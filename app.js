1daa1b479a3f702916b32b2f1489c45082d8ca92
Vitaly Yurkin <jvn9405@gmail.com>
2024-04-06 13:17:01 +0300

    Упражнение - Кошелек

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-/* преобразовать пользователей до вида
-	{ fullName: 'Вася Пупкин', skillNum: 2  }
-*/
+/* Релизовать методы увеличения и уменьшения баланса, 
+	при котором каждая операция сохраняется в массив
+	operations в виде { reason: 'Оплата налогов', sum: -100 }.
+	Возвращается true, если успешно и false, если не хватает баланса
+	Так же реализовать метод вывода числа операций по кошельку
+ */
 
-const users = [
-	{ 
-		name: 'Вася',
-		surname: 'Пупкин',
-		age: 30,
-		skills: ['Разработка', 'DevOps'] 
+const wallet = {
+	balance: 0,
+	operations: [],
+	increase: function(sum, reason) {
+		this.balance += sum;
+		this.operations.push({
+			reason: reason,
+			sum: sum
+		})
+		return true;
 	},
-	{ 
-		name: 'Катя', 
-		surname: 'Белова',
-		age: 18, 
-		skills: ['Дизайн'] 
+	decrease: function(sum, reason) {
+		if (this.balance < sum) {
+			console.log('Недостаточно баланса');
+			return false;
+		}
+		this.balance -= sum;
+		this.operations.push({
+			reason: reason,
+			sum: -sum
+		})
+		return true;
 	},
-];
+	getOperationLenght: function () {
+		return this.operations.length;
+	}
+};
 
-const userData = users.map(user => {
-	return {
-		fullName: `${user.name} ${user.surname}`,
-		skillNum: user.skills.length
-	};
-});
-console.log(userData);
+console.log(wallet.increase(1000, 'Зарплата'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(2000, 'Покупка ноутбука'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(500, 'Покупка телефон'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.balance);
+console.log(wallet.operations);
