c2c54830fb4eacc7a59d17f2ee84c46e3a8da0c9
Vitaly Yurkin <jvn9405@gmail.com>
2024-04-12 10:23:17 +0300

    Call, apply

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 'use strict';
 
-const b = 1;
+const audi = {
+	brand: 'Audi',
+	model: 'A3',
+	year: 2021,
+	damages: [],
+	addDamage(part, rate) {
+		console.log(`У авто ${this.brand} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`);
 
-const a = {
-	b,
-	getB: function() {
-		return this.b;
-	},
-	getBAlt() {
-		return this.b;
+		return this.damages.push({
+			part,
+			rate
+		});
 	}
-
 }
 
-console.log(a.getBAlt());
+audi.addDamage('Капот', 1);
+
+const bmw = {
+	brand: 'BMW',
+	model: 'X5',
+	year: 2022,
+	damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+// bmw.addDamage('Бампер', 2);
+
+const addDamageFunc = audi.addDamage;
+
+addDamageFunc.call(bmw, ...['Бампер', 2]);
+addDamageFunc.call(audi, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, ['Бампер', 2]);
+addDamageFunc.apply(audi, ['Бампер', 2]);
+
+console.log(bmw);
