c52c33bf4acea617b042a7c283a916de28e0f849
Vitaly Yurkin <jvn9405@gmail.com>
2024-04-20 16:00:01 +0300

    Выбор и манипуляция с элементами

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 'use strict';
 
-/* 
-	Сделать функицю пользовтеля, которая берёт за основу
-	userInfo и за счёт замыкания создаёт новый объект, с
-	котором можно работать как user1().increse(100)
-*/
+// Следующий уровень (задачи) - Уровень 1.7 задачника JavaScript
 
-const userInfo = {
-	balance: 0,
-	operations: 0,
-	increse(sum) {
-		this.balance +=sum;
-		this.operations++;
-	}
-}
-
-function user() {
-	const userObj = {
-		balance: 0,
-		operations: 0,
-		increse(sum) {
-			this.balance +=sum;
-			this.operations++;
-		}
-	};
-	return function() {
-		return userObj;
-	}
-}
-
-const user1 = user();
-user1().increse(100);
-user1().increse(100);
-console.log(user1());
-
-const user2 = user();
-user2().increse(100);
-console.log(user2());
+const panelText = document.querySelector('.panel').innerText;
+console.log(panelText);
+document.querySelector('.panel').innerText = 'New text';
