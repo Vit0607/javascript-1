f7cefaeb3ceab3ba2889015b5d41cfec5ced494c
Vitaly Yurkin <jvn9405@gmail.com>
2024-04-21 15:30:00 +0300

    Обработка событий с клавиатуры

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 'use strict';
 
-// Следующий уровень (задачи) - Уровень 1.7 задачника JavaScript
+// Следующий уровень (задачи) - Уровень 1.9 задачника JavaScript
 
-// document.querySelector('.button').addEventListener('click', function() {
-//   const input = document.querySelector('.input').value;
-//   if (!input) {
-//     return;
-//   }
-//   document.querySelector('.panel').innerText = input;
-//   document.querySelector('.input').value = '';
-// });
-
-function changeClick() {
+function submitForm() {
   const input = document.querySelector('.input').value;
   if (!input) {
     return;
   }
   document.querySelector('.panel').innerText = input;
   document.querySelector('.input').value = '';
+}
+
+function inputChanged(e) {
+  if (e.code == 'Enter') {
+    submitForm();
+  }
 }
